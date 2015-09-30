504745f1baa88583436eaa4a7796409ed0554e81
maiya777 <maiya777@list.ru>
2015-09-30 23:32:46 +0300

    Done

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,50 @@
+def using_push(arr,str)
+  arr.push(str)
+end
+
+def using_unshift(arr,str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+ # two_last_el=[arr[-1],arr[-2]]
+
+  arr.pop(2)
+  #two_last_el
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr,elem)
+  arr.insert(4,elem)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr,str)
+  arr.delete(str)
+end
+
+def using_delete_at(arr,int)
+  arr.delete_at(int)
+end
